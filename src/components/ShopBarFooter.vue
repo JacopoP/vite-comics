c6b9b90ece527731abfbf76cdb9e6e70ec69c26b
Jac22 <jacopo.prudentino@gmail.com>
2022-12-13 15:14:38 +0100

    Creata la barra shop del footer

---
+++ src/components/ShopBarFooter.vue
@@ -0,0 +1,92 @@
+<script>
+export default {
+    data() {
+        return {
+            shopArr: [
+                {
+                    icon: 'buy-comics-digital-comics.png',
+                    text: 'DIGITAL COMICS',
+                    link: '#',
+                },
+                {
+                    icon: 'buy-comics-merchandise.png',
+                    text: 'DC MERCHANDISE',
+                    link: '#',
+                },
+                {
+                    icon: 'buy-comics-subscriptions.png',
+                    text: 'SUBSCRIPTION',
+                    link: '#',
+                },
+                {
+                    icon: 'buy-comics-shop-locator.png',
+                    text: 'COMIC SHOP LOCATOR',
+                    link: '#',
+                },
+                {
+                    icon: 'buy-dc-power-visa.svg',
+                    text: 'DC POWER VISA',
+                    link: '#',
+                },
+            ],
+        }
+    },
+    methods: {
+        getImagePath: function (imgUrl) {
+            return new URL(imgUrl, import.meta.url).href;
+        },
+    },
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div v-for="(shopOpt, index) in shopArr" :key="index">
+                <a :href="shopOpt.link">
+                    <img :src="getImagePath('./../assets/img/' + shopOpt.icon)" :alt="shopOpt.text">
+                    {{ shopOpt.text }}
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use './../style/partials/variables.scss';
+@use './../style/partials/mixIn.scss';
+
+section {
+    background-color: variables.$blue-DC;
+    height: 150px;
+    color: #fff;
+
+    .container {
+        @include mixIn.my-display-flex;
+        @include mixIn.center(vertical);
+        justify-content: space-between;
+        height: 100%;
+
+        div {
+            width: fit-content;
+            border-radius: 2px;
+            overflow: hidden;
+
+            a {
+                padding: 5px;
+                line-height: 55px;
+                @include mixIn.center(vertical);
+                column-gap: 8px;
+
+                img {
+                    height: 55px;
+                }
+            }
+
+            &:hover {
+                background: lighten(variables.$blue-DC, 20%);
+            }
+        }
+    }
+}
+</style>
