e50988f81ffdf0c5280ae7b3fb710e7fdb809653
Jac22 <jacopo.prudentino@gmail.com>
2022-12-13 23:04:19 +0100

    Aggiunto effetto di hover alle cards

--- src/components/comicsCard.vue
+++ src/components/comicsCard.vue
@@ -9,6 +9,7 @@ export default {
 <template>
     <div class="card">
         <img :src="comic.thumb" :alt="comic.series">
+        <span>{{ comic.price }}<br>{{ comic.type }}</span>
         {{ comic.series }}
     </div>
 </template>
@@ -17,6 +18,8 @@ export default {
 .card {
     flex-basis: calc(100% / 6 - 30px);
     color: #fff;
+    position: relative;
+    cursor: pointer;
 
     img {
         width: 100%;
@@ -24,6 +27,23 @@ export default {
         object-fit: cover;
         object-position: top;
         margin-bottom: 15px;
+    }
+
+    span {
+        display: none;
+        position: absolute;
+        text-align: center;
+        top: 70px;
+        left: 50%;
+        transform: translate(-50%, 0);
+    }
+
+    &:hover img {
+        opacity: .3;
+    }
+
+    &:hover span {
+        display: block;
     }
 }
 </style>
